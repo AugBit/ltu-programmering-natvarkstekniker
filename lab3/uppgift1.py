f198a20dd7c85983c6230bf9acbf77aa027df48b
August Rydnell <august.ryd@gmail.com>
2026-02-02 15:08:37 +0100

    wip

---
+++ lab3/uppgift1.py
@@ -0,0 +1,8 @@
+# skapa lista som innehåller alla konsonanter
+# konsonanter ska dubblas runt vokal tex: "h" blir oho 
+
+# iterera igenom varje bokstav i en sträng, kolla om bokstav finns i listan med konsonanter 
+ 
+# om den finns, gör så att det blir ett o framför och o bakom 
+
+listOfKons = ["b","c","d","f","g","h","j","k","l","m","n","p","q","r","s","t","v","w","x","z","","","","","",""]
