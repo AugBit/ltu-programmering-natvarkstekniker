ac5ed01f4d255af4480e6f03a3ffb3b769fbb8b9
August Rydnell <august.ryd@gmail.com>
2026-02-04 16:00:50 +0100

    wip uppgift1

--- lab3/uppgift1.py
+++ lab3/uppgift1.py
@@ -1,25 +1,17 @@
-# skapa lista som innehåller alla konsonanter
-# konsonanter ska dubblas runt vokal tex: "h" blir hoh
 
-# iterera igenom varje bokstav i en sträng, kolla om bokstav finns i listan med konsonanter 
- 
-# om den finns, gör så att det blir ett h framför och h bakom 
+word = ""
+def rovarspraket(stringToCheck):
+    listOfKons = ["b","c","d","f","g","h","j","k","l","m","n","p","q","r","s","t","v","w","x","z","B","C","D","F","G","H","J","K","L","M","N","P","Q","R","S","T","V","W","X","Z"]
+    
+    for x in stringToCheck:
+        for y in listOfKons:
+            if x == y:
+                letter = x + "o" + x 
+                break
+            else:
+                letter = x
 
-listOfKons = ["b","c","d","f","g","h","j","k","l","m","n","p","q","r","s","t","v","w","x","z"]
-stringToCheck = "hej"
-stringToModify = ""
-addChar = True
+    word = word + letter
+    print(word)
 
-for x in stringToCheck:
-    if addChar == True:
-        print(x)
-    addChar = True
-    for y in listOfKons:
-        #print(x, " : ", y)
-        if x == y:
-            print("h" + x + "h")
-            addChar = False
-            
-
-
-print(stringToModify)
+rovarspraket("hej")
