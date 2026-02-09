67fec4692c0a168186ec2dd537872779b25331cb
August Rydnell <august.ryd@gmail.com>
2026-02-09 16:19:21 +0100

    wip uppgift 4

--- lab3/uppgift4.py
+++ lab3/uppgift4.py
@@ -1,2 +1,24 @@
+# lägga till en röst, om person inte finns ska en skapas
+def vote(dict,person):
+    if person in dict:
+        dict[person] = dict.get(person) + 1
+
+    else:
+        print("not found ")
+        dict[person] = 1
+    print(dict)
+
+
+# returnera antalet röster på person, om person inte finns returnera 0
+def votes(dict,person):
+    pass
+
+# returnera namnet på person med flest röster, om inte finns returnera ***OPEN***
+def result(dict):
+    pass
+
 # skapa dictionary som innehåller: key = namn, value = antalet röster
-# 
+names = {}
+vote(names, "pedro")
+vote(names, "pedro")
+vote(names, "pedro")
