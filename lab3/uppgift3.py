b3c10855abc67a65a5b9e0df437a7102efc47f72
August Rydnell <august.ryd@gmail.com>
2026-02-09 15:29:03 +0100

    uppgift 3 completed

---
+++ lab3/uppgift3.py
@@ -0,0 +1,15 @@
+# x = multiplicera talen 
+# antal tal = n 
+# formel = x ^ 1/n
+
+def geomean(list):
+    x = 1
+    n = 0
+    for num in list:
+        x = x * num
+        n = n + 1
+
+    mean = x ** (1 / n)
+    print(mean)
+
+geomean([1,2,3,10])
