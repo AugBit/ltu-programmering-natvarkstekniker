7d2553d666df1c08b926e7d70e6a64052c2aaa21
August Rydnell <august.ryd@gmail.com>
2026-02-09 13:31:01 +0100

    uppgift 1 finish

--- lab3/uppgift1.py
+++ lab3/uppgift1.py
@@ -1,8 +1,7 @@
-
-word = ""
 def rovarspraket(stringToCheck):
     listOfKons = ["b","c","d","f","g","h","j","k","l","m","n","p","q","r","s","t","v","w","x","z","B","C","D","F","G","H","J","K","L","M","N","P","Q","R","S","T","V","W","X","Z"]
-    
+    word = ""
+
     for x in stringToCheck:
         for y in listOfKons:
             if x == y:
@@ -11,7 +10,8 @@ def rovarspraket(stringToCheck):
             else:
                 letter = x
 
-    word = word + letter
+        word = word + letter
+
     print(word)
 
 rovarspraket("hej")
