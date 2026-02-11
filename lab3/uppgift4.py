aadf791c5d438123bc9455c8403a00d2e083e3b7
August Rydnell <august.ryd@gmail.com>
2026-02-11 15:21:11 +0100

    pretty much finished

--- lab3/uppgift4.py
+++ lab3/uppgift4.py
@@ -1,18 +1,48 @@
-# lägga till en röst, om person inte finns ska en skapas
+from collections import Counter
 def vote(dict,person):
     dict[person] = dict.get(person, 0) + 1
 
-# returnera antalet röster på person, om person inte finns returnera 0
 def votes(dict,person):
-    pass
+    score = dict.get(person, 0)
+    print(score)
+    return score 
 
-# returnera namnet på person med flest röster, om inte finns returnera ***OPEN***
 def result(dict):
-    pass
+    winner = ("***OPEN***")
+    
+    if dict == {}:
+        print(winner)
+        return winner
+    elif moreThanOneWinner(dict) == True:
+        print(winner)
+        return winner
 
-# skapa dictionary som innehåller: key = namn, value = antalet röster
+    winner = max(dict, key=dict.get)
+    print(winner)
+    return winner
+
+# räknar ut om det finns mer än 1 vinnare 
+def moreThanOneWinner(dict):
+    counter = 0
+    resultToCompare = ""
+    
+    for value in dict.values():
+        if resultToCompare == value:
+            counter = counter + 1
+        resultToCompare = value
+
+    if counter >= 1:
+        return True        
+    else:
+        return False
+
 names = {}
 vote(names, "pedro")
 vote(names, "pedro")
-vote(names, "pedro")
-print(names)
+vote(names, "august")
+vote(names, "august")
+vote(names, "august")
+votes(names, "august")
+votes(names, "pedro")
+
+result(names)
